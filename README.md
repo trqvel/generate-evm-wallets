45a313902e36438f49004e26a4ed46997aad8495
trqvel <levqrt@gmail.com>
2024-10-31 18:48:53 +0300

    final update

---
+++ README.md
@@ -0,0 +1,8 @@
+### node modules
+```npm i```
+
+### soft for generation new unique evm address
+change ```walletCount``` to generate any number of wallets
+all generated wallets are in ```wallets.csv```
+
+command to run -> ```npm run dev```
--- generateWallets.ts
+++ generateWallets.ts
@@ -1,44 +1,40 @@
-import { mnemonicToEntropy, generateMnemonic } from 'bip39';
+import { generateMnemonic, mnemonicToSeedSync } from 'bip39';
 import { privateKeyToAccount } from 'viem/accounts';
 import * as fs from 'fs';
 import * as path from 'path';
+import { HDKey } from '@scure/bip32';
+import chalk from 'chalk';
 
-async function createWalletFromMnemonic() {
-    // Генерируем случайную сид-фразу
+async function generateWallet() {
     const mnemonic = generateMnemonic();
-
-    // Конвертируем сид-фразу в энтропию и используем как приватный ключ
-    const entropy = mnemonicToEntropy(mnemonic);
-
-    // Приватный ключ создается из энтропии напрямую
-    const privateKey = `0x${entropy}` as `0x${string}`; // Приведение типа
-
-    // Получаем аккаунт на основе приватного ключа
+    const seed = mnemonicToSeedSync(mnemonic);
+    const hdkey = HDKey.fromMasterSeed(seed);
+    const node = hdkey.derive("m/44'/60'/0'/0/0");
+    const privateKey = `0x${Buffer.from(node.privateKey!).toString('hex')}` as `0x${string}`;
     const account = privateKeyToAccount(privateKey);
 
-    console.log("Адрес:", account.address);
-    console.log("Приватный ключ:", privateKey);
-    console.log("Сид-фраза:", mnemonic);
-
-    // Записываем данные в CSV
     await appendToCSV({ address: account.address, privateKey, mnemonic });
-
     return { address: account.address, privateKey, mnemonic };
+}
+
+async function createWallets(count: number) {
+    const chalk = (await import('chalk')).default;
+    for (let i = 0; i < count; i++) {
+        await generateWallet();
+    }
+    console.log(chalk.green.bold(`${count} wallets successfully generated and added to wallets.csv`));
 }
 
 async function appendToCSV(data: { address: string; privateKey: string; mnemonic: string }) {
     const filePath = path.resolve("wallets.csv");
     const csvLine = `${data.address},${data.privateKey},${data.mnemonic}\n`;
 
-    // Проверяем, существует ли файл
     if (!fs.existsSync(filePath)) {
-        // Если файл не существует, добавляем заголовки
         fs.writeFileSync(filePath, "Address,PrivateKey,Mnemonic\n");
     }
 
-    // Добавляем строку с данными в CSV
     fs.appendFileSync(filePath, csvLine);
 }
 
-// Создаем кошелек и записываем результат в CSV
-createWalletFromMnemonic().catch(console.error);
+const walletCount = 10;
+createWallets(walletCount).catch(console.error);
